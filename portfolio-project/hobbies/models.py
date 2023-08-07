1619a36dfdc704b936cff899f4f04c85bb567cdd
milomia <m-hunt3@sky.com>
2023-08-07 09:01:53 +0300

    changes for description field

--- portfolio-project/hobbies/models.py
+++ portfolio-project/hobbies/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Hobby(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=20)
+    description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.summary
--- portfolio-project/hobbies/views.py
+++ portfolio-project/hobbies/views.py
@@ -9,4 +9,4 @@ def home(request):
 
 def detail(request, hobby_id):
     hobby_detail = get_object_or_404(Hobby, pk=hobby_id)
-    return render(request, 'hobbies/detail.html', {'hobby':hobbies_detail})
+    return render(request, 'hobbies/detail.html', {'hobby':hobby_detail})
